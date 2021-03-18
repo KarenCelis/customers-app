7ea738faf0c38f583ba70b96bc0e906ee200e2e5
Karen Celis <k-celis@javeriana.edu.co>
2021-03-18 10:04:23 -0500

    init

--- spring/angular/customers-app/src/app/app.component.ts
+++ spring/angular/customers-app/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'customers-app';
+  title = 'Welcome to angular';
+  curso: String = 'Spring 5 con Angular'
+  student: String = 'Karen Celis'
 }
